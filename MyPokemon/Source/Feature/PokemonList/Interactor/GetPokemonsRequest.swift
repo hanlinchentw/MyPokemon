c6fb2adb0b0d2fd0e3772d9e9c9a3f17802f38a7
陳翰霖 <chenhanlin@Hans-MacBook-Pro.local>
2023-02-24 23:26:56 +0800

    feat: use offset & limit as pagnation params instead of nextUrl

--- MyPokemon/Source/Feature/PokemonList/Interactor/GetPokemonsRequest.swift
+++ MyPokemon/Source/Feature/PokemonList/Interactor/GetPokemonsRequest.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 struct GetPokemonRequest: HttpRequest {
+  var offset: Int
+  var limit: Int
+  
+  init(offset: Int = 0, limit: Int = 20) {
+    self.offset = offset
+    self.limit = limit
+  }
+
   var host: String { Constants.POKEMON_SERVER_HOST }
   var path: String { "/api/v2/pokemon" }
   var method: HttpMethod { .GET }
-  var queryItem: [String : String]? { nil }
+  var queryItems: [String : String]? {
+    [
+      "offset": "\(offset)",
+      "limit": "\(limit)"
+    ]
+  }
 }
